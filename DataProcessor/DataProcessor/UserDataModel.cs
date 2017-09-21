35797684f0530187b00b2392dc6f3803a33bcdb8
Mikolaj Toczek <m.toczek@hotmail.com>
2017-09-21 23:02:53 +0200

    Added DataProcessor project

---
+++ DataProcessor/DataProcessor/UserDataModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataProcessor
+{
+    [Serializable]
+    public class UserDataModel
+    {                
+        public UserDataModel()
+        {
+        }
+
+        public enum Gender
+        {
+            male,
+            female
+        }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        public Gender UserGender { get; set; }
+
+        public ushort Age { get; set; }
+    }
+}
---
+++ DataProcessor/DataProcessor/UserDataProcessing.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataProcessor
+{
+    public static class UserDataProcessing
+    {
+        public static void SaveToFile(UserDataModel userData)
+        {
+            string myDocsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            using (StreamWriter outputFile = new StreamWriter(myDocsPath + @"\UserData.txt"))
+            {
+                outputFile.WriteLine(userData.FirstName);
+                outputFile.WriteLine(userData.LastName);
+                outputFile.WriteLine(userData.Address);
+                outputFile.WriteLine(userData.UserGender);
+                outputFile.WriteLine(userData.Age);
+            }                
+        }
+    }
+}
